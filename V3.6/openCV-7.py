a188c4a4c46be4130a3d0af51d06aaee9dd72eb8
John Wilkins <wilkinsjohnstanley@gmail.com>
2025-01-27 18:27:58 -0600

    shapes pham

--- V3.6/openCV-7.py
+++ V3.6/openCV-7.py
@@ -10,6 +10,8 @@ cam.set(cv2.CAP_PROP_FOURCC,cv2.VideoWriter_fourcc(*'MJPG'))
 while True:
     ignore,  frame = cam.read()
     frame[140:220, 280:350]=(0,0,0) #This is the rectangle
+    cv2.rectangle(frame, (250, 140), (390, 220),(255,0,0), -1) #This is also a rectangle -1 for solid, positive num for empty shape
+    cv2.circle(frame, (320,180), 25, (0, 0, 0), -1) #This is a circle. 25 is radius
     cv2.imshow('my WEBcam', frame)
     cv2.moveWindow('my WEBcam',0,0)
     if cv2.waitKey(1) & 0xff ==ord('q'):

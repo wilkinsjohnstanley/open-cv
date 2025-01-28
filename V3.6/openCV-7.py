c9be3530496d73c4c302604d401d3003beaa1b8b
John Wilkins <wilkinsjohnstanley@gmail.com>
2025-01-27 18:33:47 -0600

    now we've added text

--- V3.6/openCV-7.py
+++ V3.6/openCV-7.py
@@ -2,6 +2,7 @@ import cv2
 print(cv2.__version__)
 width=1280
 height=720
+myText="John is the boss"
 cam=cv2.VideoCapture(0,cv2.CAP_DSHOW)
 cam.set(cv2.CAP_PROP_FRAME_WIDTH, width)
 cam.set(cv2.CAP_PROP_FRAME_HEIGHT,height)
@@ -12,6 +13,7 @@ while True:
     frame[140:220, 280:350]=(0,0,0) #This is the rectangle
     cv2.rectangle(frame, (250, 140), (390, 220),(255,0,0), -1) #This is also a rectangle -1 for solid, positive num for empty shape
     cv2.circle(frame, (320,180), 25, (0, 0, 0), -1) #This is a circle. 25 is radius
+    cv2.putText(frame, myText,(120,60), cv2.FONT_ITALIC, 2, (255, 0, 0), 3) #This is text. 2 is the tallness. 3 is the thickness.
     cv2.imshow('my WEBcam', frame)
     cv2.moveWindow('my WEBcam',0,0)
     if cv2.waitKey(1) & 0xff ==ord('q'):

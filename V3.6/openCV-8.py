c804a344d0e7ef1f276c3bd5f82c0710eb90cada
John Wilkins <wilkinsjohnstanley@gmail.com>
2025-01-28 10:27:47 -0600

    regions of interest

--- V3.6/openCV-8.py
+++ V3.6/openCV-8.py
@@ -1,7 +1,7 @@
 import cv2
 print(cv2.__version__)
-width=1280
-height=720
+width=640
+height=370
 cam=cv2.VideoCapture(0,cv2.CAP_DSHOW)
 cam.set(cv2.CAP_PROP_FRAME_WIDTH, width)
 cam.set(cv2.CAP_PROP_FRAME_HEIGHT,height)
@@ -11,6 +11,11 @@ while True:
     ignore,  frame = cam.read()
     #Create a region of interest.
     frameROI=frame[150:210,250:390]
+    cv2.imshow('my ROI', frameROI)
+    cv2.moveWindow('my ROI', 650, 0) #This makes sure it's not underneath the normal window.
+    #Gray scale has 1 data point per pixel. Normal has 3 data points per pixel. More quickly able to analize it.
+    
+    #normal stuff
     cv2.imshow('my WEBcam', frame)
     cv2.moveWindow('my WEBcam',0,0)
     if cv2.waitKey(1) & 0xff ==ord('q'):
